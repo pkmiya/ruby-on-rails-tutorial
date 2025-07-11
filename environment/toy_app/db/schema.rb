d37599b53aee1b7cdc04dadb31e1affbca9b0560
pkmiya <xbzd.cnlv@icloud.com>
2025-07-11 13:48:23 +0900

    add generate scaffold micropost

--- environment/toy_app/db/schema.rb
+++ environment/toy_app/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_24_060510) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_11_043952) do
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
