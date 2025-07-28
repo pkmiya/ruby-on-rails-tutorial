3f0418678336516027ec4ed640961fa194a7ea3f
pkmiya <xbzd.cnlv@icloud.com>
2025-07-28 17:48:01 +0900

    5.4.2 update users new (= signup) - fix routing

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -12,5 +12,8 @@ class SiteTest < ActionDispatch::IntegrationTest
 
     get contact_path
     assert_select "title", full_title("Contact")
+
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
 end
