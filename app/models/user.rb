9b955cea11ae0d0ba6fa16a307a9951c2cad0632
pkmiya <xbzd.cnlv@icloud.com>
2025-11-28 16:35:30 +0900

    6.2 add validation (validity, presense, length, format, uniqueness)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+    before_save { self.email = email.downcase }
+
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :email, presence: true,
+        length: { maximum: 255 },
+        format: { with: VALID_EMAIL_REGEX },
+        uniqueness: true
+  # best practice:
+  # validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
