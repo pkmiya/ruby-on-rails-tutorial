f5e6b0a70db0d17599357b1878d210204fb691a6
pkmiya <xbzd.cnlv@icloud.com>
2026-01-28 16:20:25 +0900

    revert: same argument

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,6 @@ class User < ApplicationRecord
   # @return [String]
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost)
+    BCrypt::Password.create(string, cost: cost)
   end
 end
