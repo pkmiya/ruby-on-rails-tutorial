a9a96bec180ee0cf2ddf57456cb3fc262bca2008
pkmiya <xbzd.cnlv@icloud.com>
2026-02-03 15:34:50 +0900

    10.2.3 add friendly forwarding (impl, test)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,6 +47,7 @@ class UsersController < ApplicationController
   # @return [nil]
   def logged_in_user
     unless logged_in?
+      store_location
       flash[:danger] = "Please log in."
       redirect_to login_url, status: :see_other
     end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -61,4 +61,10 @@ module SessionsHelper
   def current_user?(user)
     user && user == current_user
   end
+
+  # アクセスしようとしたURLを保存する
+  # @return [nil]
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
