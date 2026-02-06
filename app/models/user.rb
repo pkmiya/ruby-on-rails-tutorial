30fe3a4520f6cac54ec4a6a9ba81291e3fe02ba1
pkmiya <xbzd.cnlv@icloud.com>
2026-02-06 14:50:06 +0900

    11.1.2 add migration add_activation_to_users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-  before_save { self.email = email.downcase }
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true,
@@ -55,5 +55,20 @@ class User < ApplicationRecord
   # @return [nil]
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  private
+
+  # メールアドレスをすべて小文字にする
+  # @return [nil]
+  def downcase_email
+    email.downcase!
+  end
+
+  # 有効化トークンとダイジェストを作成および代入する
+  # @return [nil]
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
