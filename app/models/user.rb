d7304274c061123595f4ba10eece320b65ad343f
pkmiya <xbzd.cnlv@icloud.com>
2026-02-24 21:10:47 +0900

    11.3.1 refactor authenticated

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,12 +43,14 @@ class User < ApplicationRecord
   end
 
   # 渡されたトークンがダイジェストと一致したらtrueを返す
-  # @param [String] remember_token
+  # @param [String] attribute
+  # @param [String] token
   # @return [Boolean]
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
 
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   # ユーザーのログイン情報を破棄する
