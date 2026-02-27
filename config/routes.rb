09c028150016cafe36f9ce2dfca8ad29268e96b6
pkmiya <xbzd.cnlv@icloud.com>
2026-02-27 14:22:41 +0900

    12.1.2 add migration & password reset page & fix spell

--- config/routes.rb
+++ config/routes.rb
@@ -25,5 +25,5 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [ :edit ]
-  resources :password_reset, only: [ :new, :create, :edit, :update ]
+  resources :password_resets, only: [ :new, :create, :edit, :update ]
 end
