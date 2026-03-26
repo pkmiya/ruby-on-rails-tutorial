45fab82a30aedf4a036d0619d682ebc0b955ec13
pkmiya <xbzd.cnlv@icloud.com>
2026-03-26 12:41:15 +0900

    13.4.1 add image upload test

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -7,6 +7,20 @@ class MicropostsInterface < ActionDispatch::IntegrationTest
   end
 end
 
+class ImageUploadTest < MicropostsInterface
+  test "should have a file input field for images" do
+    get root_path
+    assert_select "input[type=file]"
+  end
+
+  test "should be able to attach an image" do
+    cont = "This micropost really ties the room together."
+    img  = fixture_file_upload("kitten.jpg", "image/jpeg")
+    post microposts_path, params: { micropost: { content: cont, image: img } }
+    assert @user.microposts.first.image.attached?
+  end
+end
+
 class MicropostsInterfaceTest < MicropostsInterface
   test "should paginate microposts" do
     get root_path
