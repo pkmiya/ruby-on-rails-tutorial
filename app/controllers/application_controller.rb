1f5e3a4e8bde417052b9354fbbf51d8d1777d9ff
pkmiya <xbzd.cnlv@icloud.com>
2026-03-27 14:25:14 +0900

    chore: fix indent

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,8 +15,8 @@ class ApplicationController < ActionController::Base
   def logged_in_user
     return if logged_in?
 
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url, status: :see_other
+    store_location
+    flash[:danger] = "Please log in."
+    redirect_to login_url, status: :see_other
   end
 end
